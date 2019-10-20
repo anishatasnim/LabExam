b5c96c9e70814078a5b2af4d36213ef048b2fd2c
anishatasnim <55645573+anishatasnim@users.noreply.github.com>
2019-10-20 11:02:07 +0600

    Add files via upload

---
+++ Input.php
@@ -0,0 +1,19 @@
+<?php
+
+   $id="";
+   $pass="";
+   $cpass="";
+   $name="";
+   $utype="";
+  if(isset($_REQUEST['submit'])){ 
+   $id=$REQUEST['id'];
+   $pass=$REQUEST['pass'];
+   $cpass=$REQUEST['cpass'];
+   $name=$REQUEST['name'];
+   
+   $myfile=fopen('Info.txt',w);
+   fwrite($myfile,$id.$pass.$cpass.$name);
+   fclose($myfile);
+   echo"done";}
+
+?>
